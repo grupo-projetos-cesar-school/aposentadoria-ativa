b7cc51d5d503e904c84e1155010f652f0a9c9e8f
romulolyra <65560137+romulolyra@users.noreply.github.com>
2020-11-30 11:37:54 -0300

    adicionar interesses ao Backup

--- backup.c
+++ backup.c
@@ -3,39 +3,58 @@
 #include <string.h>
 
 
+struct interesses {
+	char *novoInteresse;
+	struct interesses *prox;
+};
+struct interesses *primeiroI = NULL;
+
 struct lista {
 	char *name;
 	char *date;
 	char *email;
 	char *cep;
+	struct interesses *primeiroInteresse;
         struct lista *prox;
 };
-	struct lista *primeiro = NULL;
-	struct lista *atual = NULL;
+struct lista *primeiro = NULL;
 
-void inserir(char *nome, char *data, char *email, char *cep )
+void inserir(char *nome, char *data, char *email, char *cep, struct interesses *pInteresse)
 {
 
-	struct lista *link = (struct lista*) malloc(sizeof(struct lista));
+	struct lista *aux = (struct lista*) malloc(sizeof(struct lista));
 
-	link->name = nome;
-	link->date = data;
-	link->email = email;
-	link->cep = cep;
+	aux->name = nome;
+	aux->date = data;
+	aux->email = email;
+	aux->cep = cep;
+	aux-> primeiroInteresse = pInteresse;
 
-	link->prox = primeiro;
+	aux->prox = primeiro;
+	primeiro = aux;
 
-	primeiro = link;
+}
+struct interesses *inserirInteresses(char *interesse )
+{
+
+	struct interesses *novo = (struct interesses*) malloc(sizeof(struct interesses));
+
+	novo->novoInteresse = interesse;
+	novo->prox = primeiroI;
+	primeiroI = novo;
+	return primeiroI;
+
 }
 
-void printList() {
+void imprimirUser() {
    struct lista *ptr = primeiro;
    printf("\nNO STRUCT : \n ");
 
-	while(ptr != NULL) {
-		printf("| Nome = %s \n| Data = %s \n|Email = %s \n|Cep = %s",ptr->name,ptr->date,ptr->email,ptr->cep);
-		ptr = ptr->prox;
-  	}
+   //start from the beginning
+   while(ptr != NULL) {
+	printf("| Nome = %s \n| Data = %s \n|Email = %s \n|Cep = %s",ptr->name,ptr->date,ptr->email,ptr->cep);
+	ptr = ptr->prox;
+   }
 
 }
 
@@ -59,21 +78,22 @@ int main(int argc, char **argv)
 	numInteresses = argc - 6;
 
 	char *interesses[numInteresses];
+	struct interesses *primUser = (struct interesses*) malloc(sizeof(struct interesses)); //armazena primeiro interesse do usuario para cahr os proximos
+
 	int i0 = 0;
 	for(int i = 6; i < argc; i++) {
 		interesses[i0] = argv[i];
 		i0++;
 
 	}
-
-	inserir(nome, dataNascimento, email,cep);
-
-
 	printf("\n|Nome = %s\n|ID = %s\n|Nascimento = %s\n|email = %s\n|cep = %s",nome,  id, dataNascimento, email, cep);
 	for (int j = 0; j < numInteresses; j++) {
 		printf("\n|Interesse %d = %s", j+1, interesses[j]);
+		primUser = inserirInteresses(interesses[j]);
 	}
 
-	printList();
+	inserir(nome, dataNascimento, email,cep, primUser);
+
+	imprimirUser();
 
 }
